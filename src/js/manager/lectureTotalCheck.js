f90470fe5cd14066f3e1205cfcc1207ee88baba0
biyuns <cart0089@naver.com>
2026-03-22 23:09:51 +0900

    [FEATURE] 관리자 조회 페이지 js 구현

--- src/js/manager/lectureTotalCheck.js
+++ src/js/manager/lectureTotalCheck.js
@@ -1,23 +1,54 @@
 const userListData = JSON.parse(localStorage.getItem('userList'));
 const $totalLecture = document.querySelector(".tml-content-top-ct");
 
-let lectureTotalSum = 0;
-for (let i = userListData.length - 1; i >= 0; i--) {
-    if (userListData[i].role === "lecture") {
+const arrayLecture = userListData.filter((sub) => sub.role === "lecture");
+console.log(arrayLecture);
+const lectureArraySub = userListData.filter((sub) => sub.subscriptionStatus === true);
+const lectureArrayUnSub = userListData.filter((sub) => sub.subscriptionStatus === false);
+
+function lectureTotalManagement(arrayList) {
+    document.querySelectorAll('.tml-content').forEach(el => el.remove());
+    const pageMaxArray = arrayList.length < 10 ? arrayList.length : 10;
+    // 페이지네이션 관련 코드입니다. 추후 추가될 예정입니다.
+
+    // const userTotalPagination = Math.ceil(arrayList.length / 10);
+    // const usPaginationCurrentPage = 1;   
+    for (let i = pageMaxArray - 1; i >= 0; i--) {
         $totalLecture.insertAdjacentHTML('afterend',
             `<div class="tml-content">
         <p> ${i + 1} </p>
-        <p> ${userListData[i].id} </p>
-        <p> ${userListData[i].userName} </p>
-         <p> ${userListData[i].role === "lecture" ? "강사" : "수강생"} </p>
-        <p> ${userListData[i].phoneNumber} </p>
-        <p> ${userListData[i].contentCount}건 </p>
-        <p> ${userListData[i].signDate} </p>
-        <p> ${userListData[i].membershipSignDate === null ? "-" : userListData[i].membershipSignDate}</p>
-        <p> ${userListData[i].membershipStatus === true ? "구독" : "미구독"}</p>
-        <p> ${userListData[i].approveState}</p>
+        <p> ${arrayList[i].id} </p>
+        <p> ${arrayList[i].userName} </p>
+        <p> ${arrayList[i].role === "student" ? "수강생" : "강사"} </p>
+        <p> ${arrayList[i].phoneNumber} </p>
+        <p> ${arrayList[i].contentCount}건 </p>
+        <p> ${arrayList[i].signDate} </p>
+        <p> ${arrayList[i].membershipSignDate === null ? "-" : arrayList[i].membershipSignDate}</p>
+        <p> ${arrayList[i].membershipStatus === true ? "등록" : "미등록"}</p>
+        <p> ${arrayList[i].approveState}</p>
         <p> <img src="/src/assets/img/manager-suppoting-document.svg"> </p>
     </div>`
         )
-    }
+
+        const $userRoleColor = document.querySelector('.tml-content >p:nth-child(4)');
+        const $userMembershipColor = document.querySelector('.tml-content >p:nth-child(9)');
+
+        arrayList[i].role === "student" ? $userRoleColor.className += 'tml-content-role-student' : $userRoleColor.className += 'tml-content-role-lecture';
+        arrayList[i].role === "student"
+            ? (arrayList[i].subscriptionStatus === true ? $userMembershipColor.className += 'tml-content-memebership-student' : $userMembershipColor.className += 'tml-content-memebership-lecture')
+            : (arrayList[i].membershipStatus === true ? $userMembershipColor.className += 'tml-content-memebership-student' : $userMembershipColor.className += 'tml-content-memebership-lecture');
+    };
 }
+
+lectureTotalManagement(arrayLecture);
+
+const $lectureArraySub = document.querySelector('.m-fileter>p:nth-of-type(1)');
+const $lectureArrayUnSub = document.querySelector('.m-fileter>p:nth-of-type(2)');
+
+$lectureArraySub.addEventListener('click', () => {
+    lectureTotalManagement(lectureArraySub);
+})
+
+$lectureArrayUnSub.addEventListener('click', () => {
+    lectureTotalManagement(lectureArrayUnSub);
+})
--- src/js/manager/studentTotalCheck.js
+++ src/js/manager/studentTotalCheck.js
@@ -1,22 +1,51 @@
 const userListData = JSON.parse(localStorage.getItem('userList'));
 const $totalStudent = document.querySelector(".tms-content-top-ct");
 
+const arrayStudent = userListData.filter((sub) => sub.role === "student");
+const studentArraySub = userListData.filter((sub) => sub.subscriptionStatus === true);
+const studentArrayUnSub = userListData.filter((sub) => sub.subscriptionStatus === false);
 
-let studentTotalSum = 0;
-for (let i = userListData.length - 1; i >= 0; i--) {
-    if (userListData[i].role === "student") {
+function studentTotalManagement(arrayList) {
+    document.querySelectorAll('.tms-content').forEach(el => el.remove());
+    const pageMaxArray = arrayList.length < 10 ? arrayList.length : 10;
+    // 페이지네이션 관련 코드입니다. 추후 추가될 예정입니다.
+
+    // const userTotalPagination = Math.ceil(arrayList.length / 10);
+    // const usPaginationCurrentPage = 1;
+    for (let i = pageMaxArray - 1; i >= 0; i--) {
         $totalStudent.insertAdjacentHTML('afterend',
             `<div class="tms-content">
         <p> ${i + 1} </p>
-        <p> ${userListData[i].id} </p>
-        <p> ${userListData[i].userName} </p>
-        <p> ${userListData[i].role === "student" ? "수강생" : "강사"} </p>
-        <p> ${userListData[i].phoneNumber} </p>
-        <p> ${userListData[i].classCount}건 </p>
-        <p> ${userListData[i].signDate} </p>
-        <p> ${userListData[i].subscriptionSignDate === null ? "-" : userListData[i].subscriptionSignDate}</p>
-        <p> ${userListData[i].subscriptionStatus === true ? "구독" : "미구독"}</p>
-    </div>`
-        )
-    }
+        <p> ${arrayList[i].id} </p>
+        <p> ${arrayList[i].userName} </p>
+        <p> ${arrayList[i].role === "student" ? "수강생" : "강사"} </p>
+        <p> ${arrayList[i].phoneNumber} </p>
+        <p> ${arrayList[i].classCount} </p>
+        <p> ${arrayList[i].signDate} </p>
+        <p> ${arrayList[i].subscriptionSignDate === null ? "-" : arrayList[i].subscriptionSignDate}  </p>
+        <p> ${arrayList[i].subscriptionStatus === true ? "구독중" : "미구독"} </p>
+    </div>`)
+
+        const $userRoleColor = document.querySelector('.tms-content >p:nth-child(4)');
+        const $userMembershipColor = document.querySelector('.tms-content >p:nth-child(9)');
+
+        arrayList[i].role === "student" ? $userRoleColor.className += 'tms-content-role-student' : $userRoleColor.className += 'tms-content-role-lecture';
+        arrayList[i].role === "student"
+            ? (arrayList[i].subscriptionStatus === true ? $userMembershipColor.className += 'tms-content-memebership-student' : $userMembershipColor.className += 'tms-content-memebership-lecture')
+            : (arrayList[i].membershipStatus === true ? $userMembershipColor.className += 'tms-content-memebership-student' : $userMembershipColor.className += 'tms-content-memebership-lecture');
+
+    };
+
 }
+
+studentTotalManagement(arrayStudent);
+const $studentArraySub = document.querySelector('.m-fileter>p:nth-of-type(1)');
+const $studentArrayUnSub = document.querySelector('.m-fileter>p:nth-of-type(2)');
+
+$studentArraySub.addEventListener('click', () => {
+    studentTotalManagement(studentArraySub);
+})
+
+$studentArrayUnSub.addEventListener('click', () => {
+    studentTotalManagement(studentArrayUnSub);
+})
